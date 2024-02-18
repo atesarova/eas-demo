409635c7b81c72bcdf4c5c29a7dad1449ef13d5a
Alena Tesarova <alena.tesarova@purple-technology.com>
2024-02-18 14:22:59 +0100

    Add Text

--- App.tsx
+++ App.tsx
@@ -9,6 +9,10 @@ export default function App() {
       <Text>Test</Text>
       <Text>Test</Text>
       <Text>Test</Text>
+      <Text>Hello worlds</Text>
+      <Text>Hello worlds</Text>
+      <Text>Hello worlds</Text>
+      <Text>Hello worlds</Text>
     </View>
   );
 }
